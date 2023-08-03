eb8e0dc7df38963444098b2c70b411d665d9d9cd
Peter Lugalia <peter.lugalia@bbraun.com>
2023-08-03 09:46:11 +0200

    add initial file

---
+++ interface.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+import json
+
+from agent import query_agent, create_agent
+
+def decode_response(response: str) -> dict:
+    """
+     converts the string reponse from the model to dictionary obejct
+    Agrs:
+        reponse (str): response from the model
+    Returns:
+    dict: dictionary with reponse data
+    """
+    return json.loads(response)
+
+def write_response(response_dict: dict):
+    """
+    write a reposne from an agent to a streamlit app
+
+    args:
+        response_dict : the reposne from the agent
+    
+        Retruns:
+            None
+    """
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+    
+    #check if the response is a bar chart
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        df = pd.DataFrame(data)
+        df.set_index("columns",inplace = True)
+        st.bar_chart(df)
+    # check if the response is a line chart
+
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+    #check if the response is a table
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns= data["columns"])
+        st.table(df)
+
+    # interface for the app
+    st.title("Make sense of Data")
+    st.write("Please upload your CSV file below")
+    data = st.file_uploader("Upload a CSV")
+    query = st.text_area("Insert your query")
+
+    if st.button("Submit Query", ype = "primary"):
+        # create an agent from the CSV file
+        agent = create_agent(data)
+
+        # query the agent
+        response = query_agent(agent= agent, query= query)
+
+        # decode the response
+        decoded_response  = decoded_response(response)
+
+        #write the response  to the streamlit app
+        write_response(decoded_response)
+
+
+
+
---
+++ trial.py
@@ -0,0 +1,73 @@
+import os 
+from langchain import OpenAI
+from langchain.agents import create_pandas_dataframe_agent
+import pandas as pd
+from apikey import api_key
+
+def create_agent(filename: str):
+    """
+         create an agent that can access a large langauge model
+
+         args:
+            filename with the path to the cdv with the data
+         return :
+            an agent that can access and use the LLM
+    """
+    #create an OpenaAI object
+    llm = OpenAI(openai_api_key=api_key)
+
+    # read the csv file into a pandas dataframe
+    df = pd.read_csv(filename)
+
+    # create a pandas dataframe agent
+    return create_pandas_dataframe_agent(llm, df, verbose=False)
+
+def query_agent(agent, query):
+    """
+    query an agent and return the response as a string
+
+    args:
+         agent: the agent to query
+         query: the query to ask the agent
+    returns:
+        the response from the agent as a string
+    """
+    prompt = (
+        """
+            For the following query, if it requires drawing a table, reply as follows:
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+            If the query requires creating a bar chart, reply as follows:
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            If the query requires creating a line chart, reply as follows:
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            There can only be two types of chart, "bar" and "line".
+
+            If it is just asking a question that requires neither, reply as follows:
+            {"answer": "answer"}
+            Example:
+            {"answer": "The title with the highest rating is 'Gilead'"}
+
+            If you do not know the answer, reply as follows:
+            {"answer": "I do not know."}
+
+            Return all output as a string.
+
+            All strings in "columns" list and data list, should be in double quotes,
+
+            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
+
+            Lets think step by step.
+
+            Below is the query.
+            Query: 
+            """
+        + query
+    )
+
+    response = agent.run(prompt)
+
+    #convert the reponse to a string
+    return response.__str__()
